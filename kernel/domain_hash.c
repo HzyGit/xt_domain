d38dd776b355376b16d29ca15d48666081897a53
hzy <hzy.oop@gmail.com>
2015-05-23 11:33:56 +0800

    add nf_hook_ops define

--- kernel/domain_hash.c
+++ kernel/domain_hash.c
@@ -1,8 +1,12 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
-
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/list.h>
+#include <linux/slab.h>
 #include "domain_hash.h"
+
 
 /// @brief 依据ip查找域名 
 /// @retval 成功返回域名,否则返回NULL
@@ -11,12 +15,36 @@ char * domain_hash_find_name(__be32 ip){
 }
 EXPORT_SYMBOL(domain_hash_find_name);
 
+/// 钩子函数
+static unsigned int hook_fun(const struct nf_hook_ops *ops,
+		struct sk_buff *skb,
+		const struct net_device *in,
+		const struct net_device *out,
+		int (*okfn)(struct sk_buff *)){
+	return NF_ACCEPT;
+}
+
+
+/// 钩子结构
+static struct nf_hook_ops ops={
+	.owner=THIS_MODULE,
+	.hooknum=NF_INET_PRE_ROUTING,
+	.pf=AF_INET,
+	.priority=NF_IP_PRI_LAST,
+	.hook=hook_fun,
+};
+
 static int __init domain_hash_init(void){
+	if(nf_register_hook(&ops)<0){
+		pr_err("register hook_ops error\n");
+		return -1;
+	}
 	pr_info("load domain_hahs ok!\n");
 	return 0;
 }
 
 static void __exit domain_hash_exit(void){
+	nf_unregister_hook(&ops);
 	pr_info("unload domain_hash ok!\n");
 }
 
--- kernel/domain_hash.h
+++ kernel/domain_hash.h
@@ -1,3 +1,7 @@
 #ifndef _HASH_DOMAIN_H
 #define _HASH_DOMAIN_H
+
+/// @brief 依据ip查找域名 
+/// @retval 成功返回域名,否则返回NULL
+char * domain_hash_find_name(__be32 ip);
 #endif
